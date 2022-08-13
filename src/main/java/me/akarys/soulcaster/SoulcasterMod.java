9e6cc6420ab5c4e9c99a01531f618ec77651ba81
Amber Bertucci <amber@akarys.me>
2022-08-13 19:10:43 +0200

    Project setup

---
+++ src/main/java/me/akarys/soulcaster/SoulcasterMod.java
@@ -0,0 +1,13 @@
+package me.akarys.soulcaster;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SoulcasterMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("soulcaster");
+
+	@Override
+	public void onInitialize() {
+	}
+}
